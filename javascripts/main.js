acd55f75bad5deb0cad443a32c9bbfd68eb100e4
Alex Tjokro <alex@tjok.ro>
2016-11-29 08:06:07 -0800

    implemented scrollspy

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,3 +1,38 @@
 $(function() {
 
+	// Get the dimensions of the viewport
+   var width = window.innerWidth ||
+               document.documentElement.clientWidth ||
+               document.body.clientWidth;
+   var height = window.innerHeight ||
+                document.documentElement.clientHeight ||
+                document.body.clientHeight;
+
+  // Initialize Bootstrap's ScrollSpy
+  function scrollSpyInit() {
+  	$('body').scrollspy({ target: '#main-nav' });
+  }
+
+	// Add all responsive functions here
+	function respond() {
+
+	}
+
+	$(document).ready(function() {
+		respond();
+		scrollSpyInit();
+	})
+
+	$(window).load(function() {
+
+	});
+
+	$(window).resize(function() {
+		respond();
+	});
+
+	$(window).scroll(function() {
+
+	});
+
 }(jQuery));
