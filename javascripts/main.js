58a455c2691714c976b993a8eda605244c9fac08
Alex Tjokro <alex@tjok.ro>
2016-11-29 15:11:15 -0800

    Added styling codes

--- javascripts/main.js
+++ javascripts/main.js
@@ -13,22 +13,39 @@ $(function() {
   	$('body').scrollspy({ target: '#main-nav' });
   }
 
+  function headerHeight() {
+     $('header').height($(window).height() - $('.navbar').height());
+  }
+
+  function projectBackground() {
+		$('.recent-project__header--bg').height($('.recent-project__header').height() * 0.85);
+		$('.recent-project__details--bg').height($('.recent-project__details').height() * 0.85);
+	}
+
+	// For every element that needs to use matchHeight.js
+	function equalHeight() {
+		$('.pitch__content h4').matchHeight();
+		$('.why__content h4').matchHeight();
+	}
+
 	// Add all responsive functions here
 	function respond() {
-
+		headerHeight();
 	}
 
 	$(document).ready(function() {
 		respond();
-		scrollSpyInit();
+		// scrollSpyInit();
+		equalHeight();
 	})
 
 	$(window).load(function() {
-
+		projectBackground();
 	});
 
 	$(window).resize(function() {
 		respond();
+		projectBackground();
 	});
 
 	$(window).scroll(function() {
